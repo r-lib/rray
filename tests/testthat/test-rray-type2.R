4f72d35dce308f760a3d4aaca528d33fdf4186ae
DavisVaughan <davis@rstudio.com>
2018-11-07 15:02:44 -0500

    rray cast and type2 tests

---
+++ tests/testthat/test-rray-type2.R
@@ -0,0 +1,25 @@
+context("test-rray-type2")
+
+test_that("vec_type2 with same dimensions", {
+  x <- as_rray(array(1, dim = c(2, 2, 2)))
+
+  expect_equal(dim(vec_type2(x, x)), c(0, 2, 2))
+  expect_equal(vec_type2(x, x), vec_type(x))
+
+  expect_equal(dim(vec_type(x[0])), c(0, 0, 2))
+
+  expect_equal(vec_type_common(x, x), vec_type2(x, x))
+})
+
+
+test_that("Common dim is found", {
+
+  x <- as_rray(array(1, dim = c(2, 2, 2)))
+  y <- as_rray(array(1, dim = c(2, 2)))
+
+  # common dim
+  expect_equal(dim(vec_type2(x, y)), c(0, 2, 2))
+  expect_equal(dim(vec_type2(y, x)), c(0, 2, 2))
+
+  expect_equal(vec_type_common(x, y), vec_type2(x, y))
+})
