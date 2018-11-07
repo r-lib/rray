5fb1dba4c8f56d5cb93a8e587ec878f053d7c38d
DavisVaughan <davis@rstudio.com>
2018-11-07 09:09:12 -0500

    Let rray_broadcast() work with base matrices and arrays too

--- R/broadcast.R
+++ R/broadcast.R
@@ -1,5 +1,10 @@
 #' @export
 rray_broadcast <- function(x, dim) {
+  UseMethod("rray_broadcast")
+}
+
+#' @export
+rray_broadcast.default <- function(x, dim) {
   # precompute new dims
   x_dim <- vec_dim(x)
   dims <- rray_dims2(x_dim, dim)
@@ -8,4 +13,9 @@ rray_broadcast <- function(x, dim) {
 
   res <- rray_broadcast_cpp(x, dim)
   res
+}
+
+#' @export
+rray_broadcast.vctrs_rray <- function(x, dim) {
+  vec_restore(rray_broadcast.default(x, dim), x)
 }
