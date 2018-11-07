6cf4b54c27eb46c74160e0d4e3d90f83a19e2ea5
DavisVaughan <davis@rstudio.com>
2018-11-07 09:09:07 -0500

    Reexpose mtrx formatting, transpose and restore method

--- R/mtrx-vctrs-compat.R
+++ R/mtrx-vctrs-compat.R
@@ -1,14 +1,14 @@
-#' #' @export
-#' format.vctrs_mtrx <- function(x, ...) {
-#'   format(as_matrix(x))
-#' }
-#'
-#' #' @export
-#' t.vctrs_mtrx <- function(x) {
-#'   as_mtrx(t(as_matrix(x)))
-#' }
-#'
-#' #' @export
-#' vec_restore.vctrs_mtrx <- function(x, to) {
-#'   as_mtrx(x)
-#' }
+#' @export
+format.vctrs_mtrx <- function(x, ...) {
+  format(as_matrix(x))
+}
+
+#' @export
+t.vctrs_mtrx <- function(x) {
+  as_mtrx(t(as_matrix(x)))
+}
+
+#' @export
+vec_restore.vctrs_mtrx <- function(x, to) {
+  as_mtrx(x)
+}
