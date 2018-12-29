de66dd21c1e84c41ea0113587f7c26b50e475433
DavisVaughan <davis@rstudio.com>
2018-12-29 14:03:10 -0500

    Use importFrom not import for vctrs

--- R/aaa.R
+++ R/aaa.R
@@ -1,4 +1,35 @@
-#' @import vctrs
+#' @importFrom vctrs vec_size
+#' @importFrom vctrs vec_dim
+#' @importFrom vctrs vec_dims
+#' @importFrom vctrs vec_data
+#' @importFrom vctrs vec_restore
+#' @importFrom vctrs vec_slice
+#' @importFrom vctrs vec_type
+#' @importFrom vctrs vec_type_common
+#' @importFrom vctrs vec_math
+#'
+#' @importFrom vctrs vec_ptype_full
+#' @importFrom vctrs vec_ptype_abbr
+#'
+#' @importFrom vctrs stop_incompatible_cast
+#' @importFrom vctrs stop_incompatible_type
+#'
+#' @importFrom vctrs new_vctr
+#' @importFrom vctrs obj_print_data
+#'
+#' @importFrom vctrs vec_arith
+#' @importFrom vctrs vec_arith.logical
+#' @importFrom vctrs vec_arith.numeric
+#'
+#' @importFrom vctrs vec_cast
+#' @importFrom vctrs vec_cast.double
+#' @importFrom vctrs vec_cast.integer
+#' @importFrom vctrs vec_cast.logical
+#'
+#' @importFrom vctrs vec_type2
+#' @importFrom vctrs vec_type2.double
+#' @importFrom vctrs vec_type2.integer
+#' @importFrom vctrs vec_type2.logical
 NULL
 
 #' @importFrom rlang abort
