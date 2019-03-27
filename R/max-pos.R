71b927623f099850cbfe131e92420812c543cfe3
DavisVaughan <davis@rstudio.com>
2019-03-27 16:25:14 -0400

    Move `rray_max_pos()` to its own file

---
+++ R/max-pos.R
@@ -0,0 +1,43 @@
+#' Locate the position of the maximum value
+#'
+#' `rray_max_pos()` returns the integer position of the maximum value over an
+#' axis.
+#'
+#' @param x A vector, matrix, array, or rray.
+#' @param axis A single integer specifying the axis to compute along. `1`
+#' computes along rows, reducing the number of rows to 1.
+#' `2` does the same, but along columns, and so on for higher dimensions.
+#' The default of `NULL` first flattens `x` to 1-D.
+#'
+#' @examples
+#'
+#' x <- rray(c(1:10, 20:11), dim = c(5, 2, 2))
+#'
+#' # Flatten x, then find the position of the max value
+#' rray_max_pos(x)
+#'
+#' # Compute along the rows
+#' rray_max_pos(x, 1)
+#'
+#' # Compute along the columns
+#' rray_max_pos(x, 2)
+#'
+#' @export
+rray_max_pos <- function(x, axis = NULL) {
+
+  axis <- vec_cast(axis, integer())
+  validate_axis(axis, vec_dims(x))
+
+  res <- rray_max_pos_impl(x, axis)
+
+  res <- keep_dims(res, x, axis)
+
+  new_dim_names <- restore_dim_names(x, vec_dim(res))
+  res <- set_full_dim_names(res, new_dim_names)
+
+  vec_restore(res, x)
+}
+
+rray_max_pos_impl <- function(x, axis) {
+  rray_op_unary_one_cpp("argmax", x, as_cpp_idx(axis))
+}
---
+++ tests/testthat/test-max-pos.R
@@ -0,0 +1,58 @@
+context("test-max-pos")
+
+test_that("can compute max positions", {
+
+  x <- rray(1:5)
+
+  expect_equal(
+    rray_max_pos(x),
+    rray(5L)
+  )
+
+  xx <- rray_reshape(x, c(5, 1))
+
+  expect_equal(
+    rray_max_pos(xx, NULL),
+    rray(5L, c(1, 1))
+  )
+
+  # TODO - https://github.com/QuantStack/xtensor/issues/1487
+  expect_failure(
+    expect_equal(
+      rray_max_pos(xx, 1),
+      rray(5L, c(1, 1))
+    )
+  )
+
+  expect_equal(
+    rray_max_pos(xx, 2),
+    rray(rep(1L, times = 5), c(5, 1))
+  )
+
+})
+
+test_that("dimension names are kept", {
+
+  x <- rray(1:5, c(5, 1))
+  x <- set_col_names(x, "c1")
+  x <- set_row_names(x, letters[1:5])
+
+  expect_equal(
+    dim_names(rray_max_pos(x, 1)),
+    c(new_empty_dim_names(1), col_names(x))
+  )
+
+  expect_equal(
+    dim_names(rray_max_pos(x, 2)),
+    dim_names(x)
+  )
+
+  xx <- rray_broadcast(x, c(5, 2))
+  xx <- set_col_names(xx, c("c1", "c2"))
+
+  expect_equal(
+    dim_names(rray_max_pos(xx, 2)),
+    c(list(row_names(xx)), new_empty_dim_names(1))
+  )
+})
+
