730ca94d9b8178ea4b7bc15b0abc5704e2613298
DavisVaughan <davis@rstudio.com>
2019-03-28 15:30:13 -0400

    Add failing test for broadcasting to same dimension

--- tests/testthat/test-broadcast.R
+++ tests/testthat/test-broadcast.R
@@ -17,3 +17,15 @@ test_that("can broadcast up to a new dimension", {
   res <- rray_broadcast(x, c(1, 2, new_dim))
   expect_equal(vec_dim(rray_broadcast(x, c(1, 2, new_dim))), c(1, 2, new_dim))
 })
+
+# TODO - https://github.com/QuantStack/xtensor-r/issues/103
+test_that("can broadcast 2x2 to same dimension and not change output", {
+  x <- matrix(1:4, ncol = 2)
+
+  expect_failure(
+    expect_equal(
+      rray_broadcast(x, c(2, 2)),
+      x
+    )
+  )
+})
