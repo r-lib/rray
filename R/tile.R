43a3484c1e155ea4e4ffc79cf5fa99f5e30cf3c8
DavisVaughan <davis@rstudio.com>
2019-04-02 13:09:05 -0400

    Add first pass at `rray_tile()`

---
+++ R/tile.R
@@ -0,0 +1,33 @@
+#' Tile an array
+#'
+#' @param x A vector, matrix, array or rray.
+#'
+#' @param times An integer vector. The number of times to repeat the
+#' array along an axis.
+#'
+#' @examples
+#'
+#' x <- matrix(1:5)
+#'
+#' # Repeat the rows twice
+#' rray_tile(x, 2)
+#'
+#' # Repeate the rows twice and the columns three times
+#' rray_tile(x, c(2, 3))
+#'
+#' # Tile into a third dimension
+#' rray_tile(x, c(1, 2, 2))
+#'
+#' @export
+rray_tile <- function(x, times) {
+
+  times <- vec_cast(times, integer())
+
+  for (i in seq_along(times)) {
+    time <- times[i]
+    x <- rep_len(list(x), time)
+    x <- rray_bind(!!!x, axis = i)
+  }
+
+  x
+}
