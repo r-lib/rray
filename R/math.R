7ff8b14e6dd3e5f6445b5226e3a86eb69e8cf42d
DavisVaughan <davis@rstudio.com>
2019-04-04 17:03:57 -0400

    Rearrange `vec_math()`

--- R/math.R
+++ R/math.R
@@ -1,6 +1,14 @@
 #' @export
 vec_math.vctrs_rray <- function(fun, x, ...) {
-  res <- rray_op_unary_cpp(fun, x)
-  dim_names(res) <- dim_names(x)
-  vec_restore(res, x)
+
+  fun <- switch(
+    fun,
+    "abs" = vec_math_abs
+  )
+
+  fun(x, ...)
+}
+
+vec_math_abs <- function(x, ...) {
+  rray_abs(x)
 }
