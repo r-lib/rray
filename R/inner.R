848d1468ccd13152c4be22e5958a2b1770ebc5da
DavisVaughan <davis@rstudio.com>
2019-04-05 08:46:24 -0400

    Internal infrastructure for casting the _inner_ type of an object

---
+++ R/inner.R
@@ -0,0 +1,54 @@
+rray_type_inner <- function(x) {
+  vec_data(x)[0]
+}
+
+rray_type_inner2 <- function(x, y) {
+  vec_type2(rray_type_inner(x), rray_type_inner(y))
+}
+
+rray_type_inner_common <- function(..., .ptype = NULL) {
+
+  if (!vctrs::is_partial(.ptype)) {
+    return(rray_type_inner(.ptype))
+  }
+
+  args <- compact(list2(.ptype, ...))
+
+  if (length(args) == 0) {
+    ptype <- NULL
+  }
+  else if (length(args) == 1) {
+    ptype <- rray_type_inner(args[[1]])
+  }
+  else {
+    ptypes <- map(args, rray_type_inner)
+    ptype <- reduce(ptypes, rray_type_inner2)
+  }
+
+  vctrs::vec_type_finalise(ptype)
+}
+
+rray_cast_inner <- function(x, to) {
+  # same as vctrs:::shape_broadcast() in this case
+  to <- rray_reshape(to, shape_dim(x))
+
+  res <- vec_cast(vec_data(x), to)
+
+  vec_restore(res, x)
+}
+
+# Cast inputs to the same _inner_ types
+# <vctrs_rray<integer>[,2][2]> -> <vctrs_rray<double>[,2][2]>
+# <vctrs_rray<double>[,1][2]>  -> <vctrs_rray<double>[,1][2]>
+# notice that shapes haven't been cast to a common shape
+rray_cast_inner_common <- function(..., .to = NULL) {
+  args <- list2(...)
+  inner_type <- rray_type_inner_common(!!!args, .ptype = .to)
+  map(args, rray_cast_inner, to = inner_type)
+}
+
+shape_dim <- function(x) {
+  dim <- vec_dim(x)
+  dim[1] <- 0L
+  dim
+}
