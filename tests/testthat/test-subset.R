9b8ad54aa64e933779043f19719adb1b2c0212f6
DavisVaughan <davis@rstudio.com>
2019-04-09 16:55:05 -0400

    `rray_extract()` tests

--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -247,13 +247,20 @@ test_that("can't index beyond vector in a yank", {
   expect_error(rray_yank(x, 8:10), "length 8")
 })
 
-test_that("names are not kept with yank", {
+test_that("names are generally not kept with yank", {
   x <- rray(1:8, dim = c(2, 2, 2))
   x <- set_row_names(x, c("a", "b"))
   expect_equal(
     dim_names(rray_yank(x, 1)),
     new_empty_dim_names(1)
   )
+})
+
+test_that("names are kept with yank and a 1D array", {
+  x <- 1:2
+  names(x) <- c("a", "b")
+  expect_equal(names(rray_yank(x)), c("a", "b"))
+  expect_equal(names(rray_yank(x, 1)), "a")
 })
 
 test_that("can yank with a logical", {
@@ -325,7 +332,10 @@ test_that("cannot yank with non-logical matrix", {
   expect_error(rray_yank(x, matrix(1)), ">1 dimensions if it is a logical")
 })
 
-# TODO - negative yank
+test_that("can yank with a negative index", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+  expect_equal(rray_yank(x, -1), rray(2:8))
+})
 
 # ------------------------------------------------------------------------------
 # yank assign
@@ -367,31 +377,183 @@ test_that("can yank assign with base R objects", {
 })
 
 # ------------------------------------------------------------------------------
-# pluck
+# extract
 
-# test_that("extract can pull n-dim elements", {
-#   x <- as_rray(array(1:24, dim = c(3, 4, 2)))
-#
-#   expect_equal(x[[1]], rray(1))
-#   expect_equal(x[[2]], rray(2)) # col major
-#
-#   expect_equal(x[[1, 1, 1]], rray(1))
-#   expect_equal(x[[2, 1, 1]], rray(2))
-#
-#   # not enough dims
-#   expect_error(x[[1, 1]])
-# })
-#
-# test_that("extract works on 4D", {
-#   x <- as_rray(array(1:48, dim = c(3, 4, 2, 2)))
-#
-#   expect_equal(x[[1]], rray(1))
-#   expect_equal(x[[2]], rray(2)) # col major
-#
-#   expect_equal(x[[1, 1, 1, 1]], rray(1))
-#   expect_equal(x[[2, 1, 1, 1]], rray(2))
-#   expect_equal(x[[1, 1, 1, 2]], rray(25))
-#
-#   # not enough dims
-#   expect_error(x[[1, 1]])
-# })
+test_that("extract drops dimensions", {
+  x <- as_rray(array(1:24, dim = c(3, 4, 2)))
+
+  # first row
+  expect_equal(rray_extract(x, 1), rray_reshape(x[1], length(x[1])))
+
+  # 1st col of every dimension
+  expect_equal(rray_extract(x, , 1), rray_reshape(x[,1], length(x[,1])))
+
+  # first 3rd dim
+  expect_equal(rray_extract(x, , , 1), rray_reshape(x[,,1], length(x[,,1])))
+})
+
+test_that("extract allows subscripts to have >1 length", {
+  x <- rray(1:8, c(2, 2, 2))
+  expect_equal(
+    rray_extract(x, 1:2, 1),
+    rray_reshape(x[1:2,1], rray_elems(x[1:2,1]))
+  )
+})
+
+test_that("extract ignores trailing dots", {
+  x <- as_rray(array(1:24, dim = c(3, 4, 2)))
+  expect_equal(rray_extract(x, 1), rray_extract(x, 1,))
+  expect_equal(rray_extract(x, , 1), rray_extract(x, , 1,))
+  expect_equal(rray_extract(x, , , 1), rray_extract(x, , , 1,))
+})
+
+test_that("0D extracting", {
+  x <- as_rray(matrix(1:10, ncol = 2))
+  expect_equal(rray_extract(x, 0), rray(integer()))
+  expect_equal(rray_extract(x, 0, 1), rray(integer()))
+  expect_equal(rray_extract(x, , 0), rray(integer()))
+})
+
+test_that("extract never keeps dimension names", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+  nms <- list(r = c("r1", "r2"), c = c("c1", "c2"), d = c("d1", "d2"))
+  dim_names(x) <- nms
+  expect_equal(dim_names(rray_extract(x, 1)), new_empty_dim_names(1))
+})
+
+# equivalent to 0
+test_that("extract works with `NULL` as dimension", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+  expect_equal(rray_extract(x, NULL), rray(integer()))
+  expect_equal(rray_extract(x, NULL, 1), rray(integer()))
+  expect_equal(rray_extract(x, , NULL), rray(integer()))
+})
+
+test_that("extract works with base R", {
+  x <- array(1:8, dim = c(2, 2, 2))
+
+  expect_equal(
+    rray_extract(x, 1),
+    as.vector(x[1,,])
+  )
+
+  expect_equal(
+    rray_extract(x, 1:2, 1:2, 1),
+    as.vector(x[,,1])
+  )
+})
+
+test_that("can't index beyond vector in extract", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+  expect_error(rray_extract(x, 3), "length 2")
+  expect_error(rray_extract(x, 1:3), "length 2")
+})
+
+test_that("can extract with a logical", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+
+  expect_equal(rray_extract(x, TRUE), rray_reshape(x, rray_elems(x)))
+  expect_equal(rray_extract(x, FALSE), rray_reshape(x[0], 0))
+
+  expect_equal(rray_extract(x, TRUE, FALSE), rray_reshape(x[0], 0))
+
+  expect_error(rray_extract(x, c(TRUE, TRUE, TRUE)), "must have length 1 or")
+
+  expect_equal(rray_extract(x, c(TRUE, FALSE)), rray(c(1, 3, 5, 7)))
+})
+
+test_that("extract with NA (lgl)", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+
+  expect_equal(
+    rray_extract(x, NA),
+    rray_reshape(vec_na(x, n = 2), rray_elems(x))
+  )
+
+  expect_equal(
+    rray_extract(x, c(NA, NA)),
+    rray_reshape(vec_na(x, n = 2), rray_elems(x))
+  )
+
+  expect_equal(
+    rray_extract(x, c(NA, TRUE)),
+    rray_reshape(vec_c(vec_na(x, n = 1), x[2]), rray_elems(x))
+  )
+})
+
+test_that("extract with NA (int)", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+
+  expect_equal(
+    rray_extract(x, NA_integer_),
+    rray_reshape(vec_na(x, 1), rray_elems(x[1,]))
+  )
+
+  expect_equal(
+    rray_extract(x, c(NA_integer_, NA_integer_, NA_integer_)),
+    rray_reshape(vec_na(x, 3), rray_elems(x[c(1,1,1)]))
+  )
+})
+
+test_that("extract with NA (real)", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+
+  expect_equal(
+    rray_extract(x, NA_real_),
+    rray_extract(x, NA_integer_)
+  )
+})
+
+test_that("extract with character", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+  nms <- list(r = c("r1", "r2"), c = c("c1", "c2"), d = c("d1", "d2"))
+  dim_names(x) <- nms
+
+  expect_equal(
+    rray_extract(x, "r1"),
+    rray_extract(x, 1)
+  )
+
+  expect_equal(
+    rray_extract(x, c("r1", "r2")),
+    rray_extract(x, c(1, 2))
+  )
+
+  # mixed integer and character
+  expect_equal(
+    rray_extract(x, 2, "c1"),
+    rray_extract(x, 2, 1)
+  )
+})
+
+test_that("extract with character fails gracefully", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+  x <- set_row_names(x, c("r1", "r2"))
+  expect_error(rray_extract(x, "r3"), "non-existing")
+
+  expect_error(rray_extract(1, "x"), "unnamed")
+})
+
+test_that("can't extract past the dimensions of x", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+  expect_error(rray_extract(x, , , , 1), "dimension 4")
+})
+
+test_that("can use a negative extract", {
+  x <- rray(1:8, dim = c(2, 2, 2))
+
+  expect_equal(rray_extract(x, -1), rray_extract(x, 2))
+  expect_error(rray_extract(x, -3), "length 2")
+})
+
+# ------------------------------------------------------------------------------
+# extract assign
+
+# ------------------------------------------------------------------------------
+# `[[`
+
+test_that("names are never kept with [[", {
+  x <- rray(1:2)
+  names(x) <- c("a", "b")
+  expect_equal(dim_names(x[[1]]), new_empty_dim_names(1))
+})
