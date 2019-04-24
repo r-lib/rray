13f6d2f3ece556f252c872675b9072503ee48b46
DavisVaughan <davis@rstudio.com>
2019-04-24 15:30:40 -0400

    Add `rray_full_like()`

---
+++ R/full-like.R
@@ -0,0 +1,43 @@
+#' Create an array like `x`
+#'
+#' `rray_full_like()` creates an array with the same type and size as `x`, but
+#' filled with `value`.
+#'
+#' @details
+#'
+#' No dimension names will be on the result.
+#'
+#' @param x A vector, matrix, array or rray.
+#'
+#' @param value A value coercable to the same _inner_ type as `x` that will be
+#' used to fill the result (If `x` is an integer matrix, then `value` will be
+#' coerced to an integer).
+#'
+#' @examples
+#'
+#' x <- rray(1:10, c(5, 2))
+#'
+#' # Same shape and type as x, but filled with 1
+#' rray_full_like(x, 1L)
+#'
+#' # `fill` is coerced to `x` if it can be
+#' rray_full_like(x, FALSE)
+#'
+#' @export
+rray_full_like <- function(x, value) {
+
+  # not vec_size() (consider array(1, c(1, 1, 2)))
+  if (length(value) != 1) {
+    glubort("`value` must have length 1, not {length(value)}.")
+  }
+
+  # ensure fill is 1D and cast to inner type of `x`
+  value <- value[[1]]
+  value <- vec_cast(value, rray_type_inner(x))
+
+  res <- rray__full_like(x, value)
+
+  # no dim names on the result
+
+  vec_restore(res, x)
+}
---
+++ tests/testthat/test-full-like.R
@@ -0,0 +1,66 @@
+context("test-full-like")
+
+test_that("can perform a basic fill", {
+  x <- array(1:10)
+  expect_equal(
+    rray_full_like(x, 1L),
+    array(rep(1L, 10))
+  )
+})
+
+test_that("rray class is retained", {
+  x <- rray(1:10, c(5, 2))
+  expect_equal(
+    rray_full_like(x, 0),
+    rray(0L, c(5, 2))
+  )
+})
+
+test_that("can catch `value` with length >1", {
+  expect_error(rray_full_like(1, c(1, 2)), "1, not 2")
+})
+
+test_that("can coerce `value` as needed", {
+  expect_is(
+    rray_full_like(rray(1), 1L),
+    "vctrs_rray_dbl"
+  )
+
+  expect_is(
+    rray_full_like(rray(1), TRUE),
+    "vctrs_rray_dbl"
+  )
+
+  expect_error(rray_full_like(rray(1), "foo"), "Lossy cast")
+})
+
+test_that("character `value` is supported", {
+  expect_equal(rray_full_like(rray(1), "2"), rray(2))
+})
+
+test_that("arrays of >1 dimensionality are supported", {
+  expect_equal(
+    rray_full_like(rray(c(TRUE, FALSE)), array(1, c(1, 1))),
+    rray(c(TRUE, TRUE))
+  )
+
+  expect_equal(
+    rray_full_like(rray(c(TRUE, FALSE)), array(1, c(1, 1, 1))),
+    rray(c(TRUE, TRUE))
+  )
+})
+
+test_that("can fill 3D", {
+  expect_equal(
+    rray_full_like(rray(1:12, c(2, 3, 2)), 0),
+    rray(0L, c(2, 3, 2))
+  )
+})
+
+test_that("`NULL` input returns `NULL`", {
+  expect_equal(rray_full_like(NULL, 1), NULL)
+})
+
+test_that("0-length input is supported", {
+  expect_equal(rray_full_like(numeric(), 1), array(numeric()))
+})
