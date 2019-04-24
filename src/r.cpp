0023ca21990dad8cde3020189509cca224c3df92
DavisVaughan <davis@rstudio.com>
2019-04-24 12:18:59 -0400

    Use 16 not 15 as the default for `r_identical()`. Closes #115.

--- src/r.cpp
+++ src/r.cpp
@@ -2,7 +2,7 @@
 
 // (15 is equal to the default settings of identical())
 bool r_identical(SEXP x, SEXP y) {
-  return R_compute_identical(x, y, 15);
+  return R_compute_identical(x, y, 16);
 }
 
 bool r_is_null(SEXP x) {
