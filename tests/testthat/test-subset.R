85129a30259b2a381ed70a4d4bd69d9771aad1de
DavisVaughan <davis@rstudio.com>
2019-04-26 08:27:09 -0400

    Wrap the NA subsetting tests with `expect_error()` for now until it gets implemented

--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -98,42 +98,54 @@ test_that("can subset with a logical", {
 test_that("subset with NA (lgl)", {
   x <- rray(1:8, dim = c(2, 2, 2))
 
-  expect_equal(
-    rray_subset(x, NA),
-    vec_na(x, n = 2)
+  expect_error(
+    expect_equal(
+      rray_subset(x, NA),
+      vec_na(x, n = 2)
+    )
   )
 
-  expect_equal(
-    rray_subset(x, c(NA, NA)),
-    vec_na(x, n = 2)
+  expect_error(
+    expect_equal(
+      rray_subset(x, c(NA, NA)),
+      vec_na(x, n = 2)
+    )
   )
 
-  expect_equal(
-    rray_subset(x, c(NA, TRUE)),
-    vec_c(vec_na(x, n = 1), x[2])
+  expect_error(
+    expect_equal(
+      rray_subset(x, c(NA, TRUE)),
+      vec_c(vec_na(x, n = 1), x[2])
+    )
   )
 })
 
 test_that("subset with NA (int)", {
   x <- rray(1:8, dim = c(2, 2, 2))
 
-  expect_equal(
-    rray_subset(x, NA_integer_),
-    vec_na(x, 1)
+  expect_error(
+    expect_equal(
+      rray_subset(x, NA_integer_),
+      vec_na(x, 1)
+    )
   )
 
-  expect_equal(
-    rray_subset(x, c(NA_integer_, NA_integer_, NA_integer_)),
-    vec_na(x, 3)
+  expect_error(
+    expect_equal(
+      rray_subset(x, c(NA_integer_, NA_integer_, NA_integer_)),
+      vec_na(x, 3)
+    )
   )
 })
 
 test_that("subset with NA (real)", {
   x <- rray(1:8, dim = c(2, 2, 2))
 
-  expect_equal(
-    rray_subset(x, NA_real_),
-    rray_subset(x, NA_integer_)
+  expect_error(
+    expect_equal(
+      rray_subset(x, NA_real_),
+      rray_subset(x, NA_integer_)
+    )
   )
 })
 
