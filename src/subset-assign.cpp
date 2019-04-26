28ccc31c4421a1609c03114573247bc2323a2f8d
DavisVaughan <davis@rstudio.com>
2019-04-26 16:04:21 -0400

    Add error handling for assigning to 0 dimension, and non-broadcastable dimensions

--- src/subset-assign.cpp
+++ src/subset-assign.cpp
@@ -6,6 +6,40 @@
 // Required for the assignment step
 #include <xtensor/xarray.hpp>
 
+// xtensor can't handle assigning to a requested dimension of 0
+// i.e. this crashes) x[0, 1] <- 99
+// this should always be a no-op so we just check for it and return x
+bool any_zero_length_indexer(Rcpp::List indexer) {
+  int n = indexer.size();
+
+  for (int i = 0; i < n; ++i) {
+    if (Rf_length(indexer[i]) == 0) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void validate_broadcastable(Rcpp::IntegerVector x_dim, Rcpp::IntegerVector dim) {
+  Rcpp::LogicalVector ok = (x_dim == dim | x_dim == 1 | dim == 0);
+  if (Rcpp::is_true(Rcpp::any(!ok))) {
+    Rcpp::stop("Non-broadcastable dimensions.");
+  }
+}
+
+// Ensure that we can actually perform the assignment so we don't segfault
+template <class E1, class E2>
+void validate_broadcastable_shapes(E1 x, E2 to) {
+  auto x_shape = x.shape();
+  Rcpp::IntegerVector x_dim(x_shape.begin(), x_shape.end());
+
+  auto to_shape = to.shape();
+  Rcpp::IntegerVector to_dim(to_shape.begin(), to_shape.end());
+
+  validate_broadcastable(x_dim, to_dim);
+}
+
 template <typename T>
 auto rray__increase_dims_view(const xt::rarray<T>& x, const int& dims) {
 
@@ -23,9 +57,13 @@ auto rray__increase_dims_view(const xt::rarray<T>& x, const int& dims) {
 }
 
 template <typename T>
-auto rray__subset_assign_strided(const xt::rarray<T>& x,
+xt::rarray<T> rray__subset_assign_strided(const xt::rarray<T>& x,
                                  Rcpp::List indexer,
                                  Rcpp::RObject value) {
+
+  if (any_zero_length_indexer(indexer)) {
+    return x;
+  }
 
   // Convert `value` to an rarray
   const xt::rarray<T>& xt_value = SEXP(value);
@@ -41,15 +79,22 @@ auto rray__subset_assign_strided(const xt::rarray<T>& x,
   xt::xstrided_slice_vector sv = build_strided_slice_vector(indexer);
   auto x_subset_view = xt::strided_view(x_assignable, sv);
 
+  // Ensure that we can actually perform the assignment so we don't segfault
+  validate_broadcastable_shapes(xt_value_view, x_subset_view);
+
   x_subset_view = xt_value_view;
 
   return x_assignable;
 }
 
 template <typename T>
-auto rray__subset_assign_dynamic(const xt::rarray<T>& x,
+xt::rarray<T> rray__subset_assign_dynamic(const xt::rarray<T>& x,
                                  Rcpp::List indexer,
                                  Rcpp::RObject value) {
+
+  if (any_zero_length_indexer(indexer)) {
+    return x;
+  }
 
   // Convert `value` to an rarray
   const xt::rarray<T>& xt_value = SEXP(value);
@@ -65,9 +110,12 @@ auto rray__subset_assign_dynamic(const xt::rarray<T>& x,
   xt::xdynamic_slice_vector sv = build_dynamic_slice_vector(indexer);
   auto x_subset_view = xt::dynamic_view(x_assignable, sv);
 
+  // Ensure that we can actually perform the assignment so we don't segfault
+  validate_broadcastable_shapes(xt_value_view, x_subset_view);
+
   x_subset_view = xt_value_view;
 
-  return 1;
+  return x_assignable;
 }
 
 template <typename T>
