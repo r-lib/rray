2c9d847396f62fd089007b2c0f1ef3b468639622
DavisVaughan <davis@rstudio.com>
2019-04-26 09:59:33 -0400

    Add `pad()` for `[` subsetting

---
+++ R/pad.R
@@ -0,0 +1,55 @@
+#' Pad missing dimensions when subsetting
+#'
+#' `pad()` is used alongside the standard rray subsetting operator `[` (and
+#' the underlying `rray_subset()` function) to easily subset into higher
+#' dimensions without having to explictly list the intermediate commas.
+#'
+#' @examples
+#' x <- rray(1:4, c(1, 1, 2, 2))
+#'
+#' # pad() fills in the missing dimensions
+#' # essentially it adds commas automatically
+#'
+#' # second element in the 4th dimension
+#' x[pad(), 2]
+#'
+#' # vs using
+#' x[,,,2]
+#'
+#' # second element in 3rd
+#' # first element in 4th
+#' x[pad(), 2, 1]
+#'
+#' # can fill in the missing gaps too
+#' # this fills in the 2nd/3rd dims
+#' x[1, pad(), 1]
+#'
+#' # if a pad() isn't needed
+#' # because the dimensionality is already fully
+#' # specified by the indices, its ignored
+#' x_flat <- rray_reshape(x, 4)
+#' x_flat[pad(), 1]
+#' x_flat[1, pad()]
+#'
+#' # `pad()` can be used with base R
+#' # objects as well through `rray_subset()`
+#' x_arr <- as.array(x)
+#' rray_subset(x_arr, pad(), 1)
+#'
+#' @export
+pad <- function() {
+  new_pad()
+}
+
+new_pad <- function() {
+  new_vctr(1L, class = "vctrs_pad")
+}
+
+#' @export
+format.vctrs_pad <- function(x, ...) {
+  "<padding>"
+}
+
+is_pad <- function(x) {
+  inherits(x, "vctrs_pad")
+}
--- R/subset.R
+++ R/subset.R
@@ -123,6 +123,7 @@ rray_as_index2 <- function(x, ...) {
   indexer <- dots_list(..., .preserve_empty = TRUE, .ignore_empty = "trailing")
   dim <- rray_dim(x)
   dims <- rray_dims(x)
+  indexer <- expand_pad(indexer, dims)
   requested_dims <- vec_size(indexer)
   proxy_names <- dim_names(x)
 
@@ -197,3 +198,53 @@ as_xt_range <- function(x) {
   list(start = start, stop = stop)
 }
 
+expand_pad <- function(indexer, dims) {
+
+  has_pad <- map_lgl(indexer, is_pad)
+
+  count_pad <- sum(has_pad)
+
+  if (count_pad == 0L) {
+    return(indexer)
+  }
+
+  if (count_pad > 1) {
+    glubort("Only one `pad()` is allowed in a subset.")
+  }
+
+  pad_loc <- which(has_pad)
+
+  # number of dims without pad
+  requested_dims <- vec_size(indexer)
+  requested_dims_no_pad <- requested_dims - 1L
+
+  n_padding <- dims - requested_dims_no_pad
+
+  # Dimensionality subsetting error will be caught after the padding
+  # (this also ensures the error message has the right dimensionality)
+  if (n_padding < 0L) {
+    n_padding <- 0L
+  }
+
+  padding <- rep_len(list(missing_arg()), n_padding)
+
+  n_before <- pad_loc - 1L
+  if (n_before == 0L) {
+    before <- list()
+  }
+  else {
+    before <- indexer[seq_len(n_before)]
+  }
+
+  n_after <- requested_dims - pad_loc
+  if (n_after == 0L) {
+    after <- list()
+  }
+  else {
+    after <- indexer[(pad_loc + 1):(pad_loc + n_after)]
+  }
+
+  indexer <- c(before, padding, after)
+
+  indexer
+}
