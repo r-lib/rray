283d2c5c062d4876ebfc82ae3ebe0b015913c5e4
DavisVaughan <davis@rstudio.com>
2019-05-09 16:50:21 -0400

    Implement more correct `rray_duplicate_*()` functions

--- R/duplicate.R
+++ R/duplicate.R
@@ -76,48 +76,123 @@
 #'
 #' @name rray_duplicate
 #' @export
-rray_duplicate_any <- function(x, axis = 1L) {
+rray_duplicate_any <- function(x, axes = NULL) {
 
-  axis <- vec_cast(axis, integer())
-  validate_axis(axis, x)
+  axes <- check_duplicate_axes(axes, x)
 
-  if (identical(axis, 1L)) {
-    return(vec_duplicate_any(x))
-  }
+  x_split_flat <- duplicate_splitter(x, axes)
 
-  x <- rray_rotate(x, from = axis, to = 1L)
+  flat_res <- map_lgl(x_split_flat, vec_duplicate_any)
 
-  vec_duplicate_any(x)
+  res <- keep_dims(flat_res, x, axes)
+
+  new_dim_names <- restore_dim_names(dim_names(x), vec_dim(res))
+  res <- set_full_dim_names(res, new_dim_names)
+
+  vec_restore(res, x)
 }
 
 #' @rdname rray_duplicate
 #' @export
-rray_duplicate_detect <- function(x, axis = 1L) {
+rray_duplicate_detect <- function(x, axes = NULL) {
 
-  axis <- vec_cast(axis, integer())
-  validate_axis(axis, x)
+  axes <- check_duplicate_axes(axes, x)
 
-  if (identical(axis, 1L)) {
-    return(vec_duplicate_detect(x))
-  }
+  x_split_flat <- duplicate_splitter(x, axes)
 
-  x <- rray_rotate(x, from = axis, to = 1L)
+  res_lgl_lst <- map(x_split_flat, vec_duplicate_detect)
 
-  vec_duplicate_detect(x)
+  res_flat <- rray_unlist(res_lgl_lst)
+
+  res <- restore_shape(res_flat, vec_dim(x), axes)
+
+  res <- set_full_dim_names(res, dim_names(x))
+
+  vec_restore(res, x)
 }
 
 #' @rdname rray_duplicate
 #' @export
-rray_duplicate_id <- function(x, axis = 1L) {
+rray_duplicate_id <- function(x, axes = NULL) {
 
-  axis <- vec_cast(axis, integer())
-  validate_axis(axis, x)
+  axes <- check_duplicate_axes(axes, x)
 
-  if (identical(axis, 1L)) {
-    return(vec_duplicate_id(x))
+  x_split_flat <- duplicate_splitter(x, axes)
+
+  res_lgl_lst <- map(x_split_flat, vec_duplicate_id)
+
+  res_flat <- rray_unlist(res_lgl_lst)
+
+  res <- restore_shape(res_flat, vec_dim(x), axes)
+
+  res <- set_full_dim_names(res, dim_names(x))
+
+  vec_restore(res, x)
+}
+
+# ------------------------------------------------------------------------------
+
+check_duplicate_axes <- function(axes, x) {
+  axes <- vec_cast(axes, integer())
+  validate_axes(axes, x)
+
+  # should work for NULL and integer(0) cases
+  if (any(diff(axes) <= 0L)) {
+    glubort("`axes` must be unique and in ascending order.")
   }
 
-  x <- rray_rotate(x, from = axis, to = 1L)
+  if (is.null(axes)) {
+    axes <- seq_len(vec_dims(x))
+  }
 
-  vec_duplicate_id(x)
+  axes
+}
+
+rray_unlist <- function(x) {
+  unlist(x, recursive = FALSE, use.names = FALSE)
+}
+
+duplicate_splitter <- function(x, axes) {
+
+  axes_complement <- get_axes_complement(vec_dims(x), axes)
+
+  # Get reversed complement of axes
+  # These are used to split with
+  axes_complement_rev <- rev(axes_complement)
+
+  x_split <- rray_split(x, axes_complement_rev)
+
+  # Flatten because we need to treat each row as a vector, not as a single row
+  # otherwise the vctrs functions return 1 value per row
+  x_split_flat <- map(x_split, as.vector)
+
+  x_split_flat
+}
+
+# flat -> correct shape
+# by first pulling axes of interest to the front
+# in a reshape, then transposing to undo
+restore_shape <- function(x, dim, axes) {
+
+  dims <- length(dim)
+
+  axes_complement <- get_axes_complement(dims, axes)
+
+  permutation <- c(axes, axes_complement)
+
+  out <- rray_reshape(x, dim[permutation])
+  out <- rray_transpose(out, order(permutation))
+
+  out
+}
+
+get_axes_complement <- function(dims, axes) {
+  axes_seq <- seq_len(dims)
+
+  if (length(axes) == 0L) {
+    axes_seq
+  }
+  else {
+    axes_seq[-axes]
+  }
 }
