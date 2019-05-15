47784d39a7dec62fe99f9e37fe2dc5781fc17a72
DavisVaughan <davis@rstudio.com>
2019-05-15 17:11:24 -0400

    No longer special case length 0 arguments

--- R/bind2.R
+++ R/bind2.R
@@ -11,11 +11,6 @@ rray_bind2 <- function(..., axis) {
   out <- rray_type_inner_common(!!!args)
   args <- map(args, rray_cast_inner, to = out)
 
-  # Remove length 0 args, which have no impact on the result
-  # besides the type, which has been captured
-  is_zero <- map_lgl(args, function(arg) length(arg) == 0L)
-  args <- args[!is_zero]
-
   dims <- compute_dims(args, as_cpp_idx(axis))
 
   # out must have the correct number of dimensions for xt::resize()
