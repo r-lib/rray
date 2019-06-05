eca7e28e0243f75edea4969e43fe8743d4e5a4de
DavisVaughan <davis@rstudio.com>
2019-06-05 10:01:09 -0400

    Reorganize `vec_math()` functions

--- R/compat-vctrs-math.R
+++ R/compat-vctrs-math.R
@@ -97,3 +97,82 @@ rray_trunc_vctrs_wrapper <- function(x, ...) {
 
   rray_trunc(x)
 }
+
+# ------------------------------------------------------------------------------
+# Summary group generic wrappers
+
+rray_all_vctrs_wrapper <- function(x, na.rm) {
+  vec_math_base("all", vec_data(x), na.rm = na.rm)
+}
+
+rray_any_vctrs_wrapper <- function(x, na.rm) {
+  vec_math_base("any", vec_data(x), na.rm = na.rm)
+}
+
+rray_range_vctrs_wrapper <- function(x, na.rm) {
+  vec_math_base("range", vec_data(x), na.rm = na.rm)
+}
+
+rray_prod_vctrs_wrapper <- function(x, na.rm) {
+  vec_math_base("prod", vec_data(x), na.rm = na.rm)
+}
+
+rray_sum_vctrs_wrapper <- function(x, na.rm) {
+  vec_math_base("sum", vec_data(x), na.rm = na.rm)
+}
+
+# ------------------------------------------------------------------------------
+# Math group generic wrappers
+
+rray_cummax_vctrs_wrapper <- function(x) {
+  vec_math_base("cummax", vec_data(x))
+}
+
+rray_cummin_vctrs_wrapper <- function(x) {
+  vec_math_base("cummin", vec_data(x))
+}
+
+rray_cumsum_vctrs_wrapper <- function(x) {
+  vec_math_base("cumsum", vec_data(x))
+}
+
+rray_cumprod_vctrs_wrapper <- function(x) {
+  vec_math_base("cumprod", vec_data(x))
+}
+
+# ------------------------------------------------------------------------------
+# Technically min() and max() are in the math generic too, but there is a
+# min.vctrs_vctr method that we need to override, unlike the other functions
+
+# xtfrm.vctrs_vctr() calls vec_proxy_compare(), which (as of 2019-06-04)
+# converts arrays to data frames, varying the first axis fastest. This makes
+# sense for the `vec_order()` functions, but results in bad behavior for
+# arrays because we want a global xtfrm, not a rowwise one. Because
+# min.vctrs_vctr uses xtfrm, this behavior leaks into these functions as well.
+# So, for example, it will return 2 to indicate that the second row is the
+# "min" row and then min.vctrs_vctr would just return the first value in the
+# second row. This is definitely not the right behavior, as we want the
+# global minimum of the array
+
+# We do borrow from vctrs a bit and ignore the `...`
+# but otherwise we fallback to the base R method and then return an rray
+
+#' @export
+min.vctrs_rray <- function(x, ..., na.rm = FALSE) {
+  vec_math_base("min", vec_data(x), na.rm = na.rm)
+}
+
+#' @export
+max.vctrs_rray <- function(x, ..., na.rm = FALSE) {
+  vec_math_base("max", vec_data(x), na.rm = na.rm)
+}
+
+#' @export
+xtfrm.vctrs_rray <- function(x) {
+  vec_data(x)
+}
+
+#' @export
+xtfrm.vctrs_rray_lgl <- function(x) {
+  vec_cast_inner(vec_data(x), integer())
+}
--- tests/testthat/test-compat-vctrs-math.R
+++ tests/testthat/test-compat-vctrs-math.R
@@ -164,3 +164,85 @@ test_that("broadcasts input using vctrs", {
 test_that("always uses `na.rm = TRUE`", {
   expect_equal(sum(rray(c(NA, 1L)), na.rm = FALSE), 1)
 })
+
+# ------------------------------------------------------------------------------
+context("test-base-cummax")
+
+test_that("vctrs dispatch works", {
+  x <- rray(5:1)
+  expect_equal(cummax(x), cummax(vec_data(x)))
+})
+
+test_that("flattens 2D+ arrays", {
+  x <- rray(5:1, c(5, 1))
+  expect_equal(cummax(x), rep(5L, 5))
+})
+
+test_that("keeps names if x is 1D", {
+  x <- rray(5:1, dim_names = list(letters[1:5]))
+  expect_equal(rray_dim_names(cummax(x)), rray_dim_names(x))
+})
+
+# ------------------------------------------------------------------------------
+context("test-base-cummin")
+
+test_that("vctrs dispatch works", {
+  x <- rray(1:5)
+  expect_equal(cummin(x), cummin(vec_data(x)))
+})
+
+test_that("flattens 2D+ arrays", {
+  x <- rray(1:5, c(5, 1))
+  expect_equal(cummin(x), rep(1L, 5))
+})
+
+test_that("keeps names if x is 1D", {
+  x <- rray(1:5, dim_names = list(letters[1:5]))
+  expect_equal(rray_dim_names(cummin(x)), rray_dim_names(x))
+})
+
+# ------------------------------------------------------------------------------
+context("test-base-cumsum")
+
+test_that("vctrs dispatch works", {
+  x <- rray(1:5)
+  expect_equal(cumsum(x), cumsum(vec_data(x)))
+})
+
+test_that("flattens 2D+ arrays", {
+  x <- rray(1:5, c(5, 1))
+  expect_equal(cumsum(x), cumsum(1:5))
+})
+
+test_that("keeps names if x is 1D", {
+  x <- rray(1:5, dim_names = list(letters[1:5]))
+  expect_equal(rray_dim_names(cumsum(x)), rray_dim_names(x))
+})
+
+test_that("integer overflow throws a warning", {
+  x <- rray(c(2147483647L, 1L))
+  expect_warning(cumsum(x), "integer overflow")
+})
+
+# ------------------------------------------------------------------------------
+context("test-base-cumprod")
+
+test_that("vctrs dispatch works", {
+  x <- rray(1:5)
+  expect_equal(cumprod(x), cumprod(vec_data(x)))
+})
+
+test_that("flattens 2D+ arrays", {
+  x <- rray(1:5, c(5, 1))
+  expect_equal(cumprod(x), cumprod(1:5))
+})
+
+test_that("keeps names if x is 1D", {
+  x <- rray(1:5, dim_names = list(letters[1:5]))
+  expect_equal(rray_dim_names(cumprod(x)), rray_dim_names(x))
+})
+
+test_that("a double is returned so no integer overflow occurs", {
+  x <- rray(c(2147483647L, 2L))
+  expect_equal(storage.mode(cumprod(x)), "double")
+})
