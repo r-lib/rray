d43430902e75197963792a51b5b8baf54632bee7
DavisVaughan <davis@rstudio.com>
2019-06-10 15:24:32 -0400

    Rework `rray_yank()` documentation

--- R/yank.R
+++ R/yank.R
@@ -1,75 +1,86 @@
-#' Get or set elements of an array by position
+#' Get or set elements of an array _by position_
 #'
-#' `rray_yank()` is the counterpart to [rray_extract()]. It extracts elements
-#' from an array _by position_. It _always_ drops dimensions
-#' (unlike [rray_subset()]), and a 1D vector is always returned. It powers
-#' the `[[` method for rrays.
+#' @description
+#'
+#' - `rray_yank()` gets and sets elements from an array _by position_. It is the
+#' complement to [rray_extract()], which gets and sets elements _by index_.
+#'
+#' - For rrays, `[[` is powered by `rray_yank()`, and allows you to
+#' select multiple elements by position with the syntax: `x[[i]]`.
+#'
+#' - There are three assignment variations that all work essentially the same.
+#' They ensures that `value` has the same inner type as `x`, and `value` must be
+#' 1D.
+#'    - `rray_yank(x, i) <- value`
+#'    - `x[[i]] <- value`
+#'    - `rray_yank_assign(x, i, value)`
 #'
 #' @param x A vector, matrix, array or rray.
 #'
 #' @param i One of the following:
 #' - An integer vector specifying the positions of the elements to yank.
-#' - A 1D logical vector of length 1, or `rray_elems(x)`.
-#' - A logical with the same dimension as `x`.
+#' - A 1D logical vector of either length 1 or `rray_elems(x)`.
+#' - A logical with the exact same dimensions as `x`.
 #'
 #' @param value A 1D value to be assigned to the location yanked by `i`. It will
-#' be cast to the type and length of `x` after being yanked by `i`.
+#' be cast to the type of `rray_yank(x, i)`.
 #'
 #' @param ... Not used. An error is thrown if extra arguments are supplied here.
 #'
 #' @details
 #'
-#' Dimension names are _only_ kept in the special case of calling `rray_yank()`
-#' on a 1D object. Otherwise, the method of keeping them is not well defined.
-#'
-#' `rray_yank()` works with base R objects.
-#'
 #' `rray_yank()` is meant as a replacement for the traditional behavior of
-#' `x[i]` since `[` for rray objects is much stricter. Separating this special
-#' behavior into a different function is less surprising.
+#' `x[i]`, which extracts multiple elements by their position, and returns a
+#' 1D vector. rray is much stricter, and for rrays `x[i]` would return the
+#' `i` rows of `x`, without dropping any dimensions. Separating this special
+#' behavior of extracting by position into a new function is less surprising,
+#' and allows `[` to be more consistent and stable.
 #'
-#' Additionally, base R has `x[[i]]` which restricts `i` to be length 1.
-#' For rray objects, `[[` acts more like `x[i]`, always dropping to 1D, but
-#' allowing for the selection of multiple positions.
+#' `rray_yank()` never keeps dimension names. For >1D objects, this would not
+#' be well defined to begin with, so the decision was made to keep this behavior
+#' for 1D objects as well. Think of `rray_yank()` as a way to rip out the inner
+#' elements of `x`. The dimension names and outer type are not a part of
+#' this information.
 #'
-#' You _cannot_ do `x[[i, j, ...]]` with rrays. For that behavior,
+#' @section The Double Bracket `[[`:
+#'
+#' `rray_yank()` powers `[[` for rray objects. It works a bit differently from
+#' base R. As with `[`, base R allows `[[` to perform two roles. It can extract
+#' _one_ element by position with `x[[i]]`, or _one_ element by index with
+#' `x[[i, j, ...]]`. This felt too flexible, so with rray objects `[[` is
+#' directly powered by `rray_yank()`, meaning it can only do `x[[i]]`. However,
+#' multiple values of `i` are allowed, rather than just 1, meaning that
+#' `x[[c(3, 5)]]` will extract the 3rd and 5th positions in `x` and return
+#' them as a 1D array.
+#'
+#' Notably this means that the index extraction behavior of `x[[i, j, ...]]`
+#' is missing with `[[` for rrays. If you want that behavior,
 #' see [rray_extract()].
 #'
 #' @examples
 #' x <- rray(10:17, c(2, 2, 2))
 #'
-#' # Resulting dimension is always 1D
+#' # Resulting dimension is always 1D, and is a base R array
 #' rray_yank(x, 1:3)
 #'
-#' # With logical
+#' # Subsetting with a logical is possible if it is either
+#' # length 1 or the length of `x`
 #' rray_yank(x, FALSE)
 #' rray_yank(x, rep(c(TRUE, FALSE), times = rray_elems(x) / 2))
 #'
 #' # You can assign a 1D vector to these yanked selections
+#' # Length 1 values are recycled as required
 #' rray_yank(x, c(1, 3, 5)) <- 9
 #'
-#' # Logicals with the same dim as `x`
-#' # can also be used as a yank indexer
-#' lgl <- rray(c(TRUE, FALSE), c(2, 2, 2))
-#' rray_yank(x, lgl)
+#' # `rray_yank()` powers `[[` as well
+#' # Notably, you can yank multiple values in `[[`
+#' x[[c(1, 3, 5)]] <- NA
 #'
-#' # And you can set elements in these locations
-#' rray_yank(x, lgl) <- NA
+#' # Logicals with the same dim as `x` can also be used as a yank indexer
+#' # This comes in handy as a way to remove NA values
+#' x[[is.na(x)]] <- 0
 #'
-#' # `[[` for rray objects is powered by
-#' # rray_yank().
-#' # This can be very useful for
-#' # performing assignment
-#' # by position.
-#' x[[c(1, 3)]] <- NA
-#'
-#' # Logical arrays with the same shape as `x`
-#' # can be assigned to. This is a useful way
-#' # to get rid of NA values.
-#' idx <- array(is.na(as.vector(x)), c(2, 2, 2))
-#'
-#' x[[idx]] <- 0
-#'
+#' @name rray_yank
 #' @family rray subsetters
 #' @export
 rray_yank <- function(x, i) {
