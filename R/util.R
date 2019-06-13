5b333d11b12f46f56d1f561208170e13196408e6
DavisVaughan <davis@rstudio.com>
2019-06-13 19:58:50 -0400

    Remove unused helper

--- R/util.R
+++ R/util.R
@@ -7,20 +7,6 @@ glubort <- function(..., .sep = "", .envir = parent.frame()) {
   abort(glue::glue(..., .sep = .sep, .envir = .envir))
 }
 
-# we need a helper to just set the dim because we
-# override dim<- to broadcast. Otherwise without this
-# we get an infinite loop because broadcast would call dim<-
-set_dim <- function(x, dim) {
-
-  if (is.null(x)) {
-    return(NULL)
-  }
-
-  attr(x, "dim") <- dim
-
-  x
-}
-
 as_cpp_idx <- function(x) {
 
   if (is.null(x)) {
