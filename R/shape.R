4958f8ba0b243668bd2862df431dadf00aef5065
DavisVaughan <davis@rstudio.com>
2019-06-14 16:03:52 -0400

    Remove `rray_size2()` helper in place of using the more correct `rray_dim2()`

--- R/shape.R
+++ R/shape.R
@@ -4,25 +4,5 @@ rray_shape <- function(x) {
 }
 
 rray_shape2 <- function(x, y) {
-  shape <- dim2(rray_shape(x), rray_shape(y))
-  map2_int(shape$x, shape$y, rray_size2)
-}
-
-# this is vctrs:::vec_size2
-rray_size2 <- function (nx, ny) {
-  if (nx == ny) {
-    nx
-  }
-  else if (nx == 0L || ny == 0L) {
-    0L
-  }
-  else if (nx == 1L) {
-    ny
-  }
-  else if (ny == 1L) {
-    nx
-  }
-  else {
-    abort(paste0("Incompatible lengths: ", nx, ", ", ny))
-  }
+  rray_dim2(rray_shape(x), rray_shape(y))
 }
