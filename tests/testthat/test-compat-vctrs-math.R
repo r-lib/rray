ce067dc0c5549ca2710ec356ff04feb0b24221a8
DavisVaughan <davis@rstudio.com>
2019-06-24 11:38:33 -0400

    Move finite tests to compat math

--- tests/testthat/test-compat-vctrs-math.R
+++ tests/testthat/test-compat-vctrs-math.R
@@ -287,3 +287,40 @@ test_that("can compute the determinant using the base R functions", {
   expect_equal(det(x), -10)
   expect_equal(determinant(x), determinant(as.matrix(x)))
 })
+
+# ------------------------------------------------------------------------------
+context("test-is-nan")
+
+test_that("vctrs dispatch works", {
+  nms <- list(NULL, "c1")
+  x <- rray(c(1, NaN, 2), c(3, 1), dim_names = nms)
+  expect <- rray(c(FALSE, TRUE, FALSE), c(3, 1), dim_names = nms)
+  expect_equal(is.nan(x), expect)
+})
+
+# ------------------------------------------------------------------------------
+context("test-is-finite")
+
+test_that("vctrs dispatch works", {
+  nms <- list(NULL, "c1")
+  x <- rray(c(1, NaN, 2), c(3, 1), dim_names = nms)
+  expect <- rray(c(TRUE, FALSE, TRUE), c(3, 1), dim_names = nms)
+  expect_equal(is.finite(x), expect)
+
+  y <- rray(c(1, Inf, 2), c(3, 1), dim_names = nms)
+  expect_equal(is.finite(y), expect)
+})
+
+# ------------------------------------------------------------------------------
+context("test-is-infinite")
+
+test_that("vctrs dispatch works", {
+  nms <- list(NULL, "c1")
+  x <- rray(c(1, NaN, 2), c(3, 1), dim_names = nms)
+  expect <- rray(c(FALSE, FALSE, FALSE), c(3, 1), dim_names = nms)
+  expect_equal(is.infinite(x), expect)
+
+  y <- rray(c(1, Inf, 2), c(3, 1), dim_names = nms)
+  expect <- rray(c(FALSE, TRUE, FALSE), c(3, 1), dim_names = nms)
+  expect_equal(is.infinite(y), expect)
+})
