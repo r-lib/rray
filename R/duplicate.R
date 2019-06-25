276b8393b0a579a11def396c5ed2a66212dc5781
DavisVaughan <davis@rstudio.com>
2019-06-24 21:17:37 -0400

    Move `keep_dims()` to `duplicate.R`. Closes #235

--- R/duplicate.R
+++ R/duplicate.R
@@ -228,3 +228,20 @@ get_axes_complement <- function(dim_n, axes) {
     axes_seq[-axes]
   }
 }
+
+keep_dims <- function(res, x, axis) {
+
+  new_dim <- rray_dim(x)
+
+  if (is.null(axis)) {
+    new_dim[] <- 1L
+  }
+  else {
+    new_dim[axis] <- 1L
+  }
+
+  res <- rray_reshape(res, new_dim)
+
+  res
+}
+
