4cb2524f91a729927c38578b505aefa9c8ef0c53
DavisVaughan <davis@rstudio.com>
2019-06-24 21:15:43 -0400

    Move `is.na()` to `compat-base.R`. Closes #236

--- R/compat-base.R
+++ R/compat-base.R
@@ -55,3 +55,20 @@ xtfrm.vctrs_rray_lgl <- function(x) {
 determinant.vctrs_rray <- function(x, logarithm = TRUE, ...) {
   determinant(as.matrix(x), logarithm = logarithm, ...)
 }
+
+# ------------------------------------------------------------------------------
+
+#' @export
+is.na.vctrs_rray <- function(x) {
+  res <- is.na(vec_data(x))
+  vec_cast_container(res, x)
+}
+
+# Currently, I am choosing to use the vctrs implementation of
+# `is.na<-vctrs_vctr`. It assigns entire rows to `NA`, and
+# only accepts logicals of either length 1, or as long
+# as the number of rows in `x`.
+# We test for this in `test-compat-base.R`
+
+# ------------------------------------------------------------------------------
+
--- tests/testthat/test-compat-base.R
+++ tests/testthat/test-compat-base.R
@@ -141,3 +141,60 @@ test_that("can compute the determinant using the base R functions", {
   expect_equal(det(x), -10)
   expect_equal(determinant(x), determinant(as.matrix(x)))
 })
+
+# ------------------------------------------------------------------------------
+context("test-is-na")
+
+test_that("can check for missing values", {
+  x <- rray(c(1, NA, 2), c(3, 1))
+  expect_equal(is.na(x), rray(c(FALSE, TRUE, FALSE), c(3, 1)))
+})
+
+test_that("dim names are retained", {
+  x <- rray(c(1, NA, 2), c(3, 1), dim_names = list(NULL, "c1"))
+  expect_equal(
+    rray_dim_names(is.na(x)),
+    rray_dim_names(x)
+  )
+})
+
+# ------------------------------------------------------------------------------
+context("test-is-na-assignment")
+
+test_that("assigning NA works with entire rows", {
+  x <- rray(c(1, 3, 2), c(3, 1, 2))
+  is.na(x) <- c(TRUE, FALSE, TRUE)
+
+  expect_equal(
+    x,
+    rray(c(NA, 3, NA), c(3, 1, 2))
+  )
+})
+
+test_that("can use a logical of length 1", {
+  x <- rray(c(1, 3, 2), c(3, 1, 2))
+  is.na(x) <- TRUE
+
+  expect_equal(x, rray(NA_real_, c(3, 1, 2)))
+})
+
+test_that("logical must recycle", {
+  x <- rray(c(1, 3, 2), c(3, 1, 2))
+  expect_error(is.na(x) <- c(TRUE, FALSE))
+})
+
+test_that("cannot use positional NA assignment", {
+  x <- rray(c(1, 3, 2), c(3, 1, 2))
+  expect_error(is.na(x) <- c(1, 2))
+})
+
+test_that("dim names are retained when assigning NAs", {
+  x <- rray(c(1, 3, 2), c(3, 1), dim_names = list(NULL, "c1"))
+  is.na(x) <- c(TRUE, FALSE, TRUE)
+  expect_equal(
+    rray_dim_names(x),
+    list(NULL, "c1")
+  )
+})
+
+# ------------------------------------------------------------------------------
